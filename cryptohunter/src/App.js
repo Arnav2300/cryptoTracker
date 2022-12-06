9218de2549c90b8c5f4ea00f5453850b2b6d5d75
arnavsar <arnav.saraswat@admybrand.com>
2022-12-07 01:23:21 +0530

    MUI and RouterDOM Installed
    made changes to app.css and created header component

--- cryptohunter/src/App.js
+++ cryptohunter/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { BrowserRouter } from "react-router-dom"
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
